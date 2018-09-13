b668be279072e4e15c912fd730b339fd22645a3e
Lally, Kelsey R <klally@hk223-27728.local>
2018-09-13 10:11:49 -0700

    Initial Commit

---
+++ Inheritance Fun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Inheritance Fun
+//
+//  Created by Lally, Kelsey R on 9/13/18.
+//  Copyright © 2018 Lally, Kelsey R. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
